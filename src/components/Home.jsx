318d01ddbf08cdb2f54f846c1b7f504c9e225edf
Hrudhey <hrudhey@gmail.com>
2025-01-21 16:34:01 +0530

    second commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -32,10 +32,10 @@ const Home = () => {
         <TableHead>
           <TableRow>
             <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell align="left">Calories</TableCell>
+            <TableCell align="left">Fat&nbsp;(g)</TableCell>
+            <TableCell align="left">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="left">Protein&nbsp;(g)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -47,10 +47,10 @@ const Home = () => {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="left">{row.calories}</TableCell>
+              <TableCell align="left">{row.fat}</TableCell>
+              <TableCell align="left">{row.carbs}</TableCell>
+              <TableCell align="left">{row.protein}</TableCell>
             </TableRow>
           ))}
         </TableBody>
